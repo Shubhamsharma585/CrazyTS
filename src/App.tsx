7dacc9d8c1e587d1bfeba7eea2a0d65c09355810
shubham sharma <shubhamsharma585@gmail.com>
2021-10-06 15:54:24 +0530

    sss

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and nsave to reload.
+          Edit <code>srgfghc/App.tsx</code> and nsave to reload.
         </p>
         <a
           className="App-link"
